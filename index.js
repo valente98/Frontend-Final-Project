241d16ca6de32e69bc55220b9f1b4732d22a8a33
valente alvarez <valvarez@basecampcodingacademy.org>
2017-11-16 10:58:23 -0600

    deleted a function that is not needed. created an array of cleats.

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-// ***************************** text slide function ***************/
-var slides = document.querySelectorAll('#slides .slide');
-console.log(slides);
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 4000);
-
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
+//*************************** Cleats inventory *********************/
+var CLEATS = [
+    {
+        nike: [
+            {
+                fireAndIce: [
+                    {
+                        img_url: '/assets/photos/redHypervenom.jpg',
+                        name:
+                            'Nike Hypervenom Phantom III DF Soccer Cleat-University',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/redMagista.jpg',
+                        name:
+                            'Nike Magista Obra II FG Soccer Cleat-Black/White/Hyper',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/redSuperfly.jpg',
+                        name:
+                            'Nike Mercurial Superfly V FG Soccer Cleat - Bright Crimson/Black/University Red/Hyper Crimson ',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/redTiempo.jpg',
+                        name:
+                            'Nike Tiempo Legend VII FG Soccer Cleat - Univeristy Red/White/Black/Bright Crimson',
+                        price: '229.99'
+                    },
+                    {
+                        img_url: '/assets/photos/blueHypervenom.jpg',
+                        name:
+                            'Nike Hypervenom Phantom III DF FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/blueMagista.jpg',
+                        name:
+                            'Nike Magista Obra II FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/blueSuperfly.jpg',
+                        name:
+                            'Nike Mercurial Superfly V FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                        price: '299.99'
+                    },
+                    {
+                        img_url: '/assets/photos/blueTiempo.jpg',
+                        name:
+                            'Nike Tiempo Legend VII FG Soccer Cleat - Gamma Blue/White/Obsidian/Glacier Blue',
+                        price: '229.99'
+                    }
+                ]
+            }
+        ]
+    }
+];
 //**************************** image slide function ******************/
 var myIndex = 0;
 
@@ -28,7 +76,6 @@ function carousel() {
 //********************** Main *********************************/
 function main() {
     carousel();
-    nextSlide();
 }
 
 $(main);
