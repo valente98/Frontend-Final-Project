efff09f108a0cb9fff128d9071ee59b623066c33
valente alvarez <valvarez@basecampcodingacademy.org>
2017-11-16 13:41:17 -0600

    created an array of objects that will keep up with the items.

--- index.js
+++ index.js
@@ -1,62 +1,284 @@
 //*************************** Cleats inventory *********************/
-var CLEATS = [
-    {
-        nike: [
-            {
-                fireAndIce: [
-                    {
-                        img_url: '/assets/photos/redHypervenom.jpg',
-                        name:
-                            'Nike Hypervenom Phantom III DF Soccer Cleat-University',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/redMagista.jpg',
-                        name:
-                            'Nike Magista Obra II FG Soccer Cleat-Black/White/Hyper',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/redSuperfly.jpg',
-                        name:
-                            'Nike Mercurial Superfly V FG Soccer Cleat - Bright Crimson/Black/University Red/Hyper Crimson ',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/redTiempo.jpg',
-                        name:
-                            'Nike Tiempo Legend VII FG Soccer Cleat - Univeristy Red/White/Black/Bright Crimson',
-                        price: '229.99'
-                    },
-                    {
-                        img_url: '/assets/photos/blueHypervenom.jpg',
-                        name:
-                            'Nike Hypervenom Phantom III DF FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/blueMagista.jpg',
-                        name:
-                            'Nike Magista Obra II FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/blueSuperfly.jpg',
-                        name:
-                            'Nike Mercurial Superfly V FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
-                        price: '299.99'
-                    },
-                    {
-                        img_url: '/assets/photos/blueTiempo.jpg',
-                        name:
-                            'Nike Tiempo Legend VII FG Soccer Cleat - Gamma Blue/White/Obsidian/Glacier Blue',
-                        price: '229.99'
-                    }
-                ]
-            }
-        ]
-    }
-];
+var CLEATS = {
+    nike: [
+        {
+            cleat: [
+                {
+                    img_url: '/assets/photos/redHypervenom.jpg',
+                    name:
+                        'Nike Hypervenom Phantom III DF Soccer Cleat-University',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/redMagista.jpg',
+                    name:
+                        'Nike Magista Obra II FG Soccer Cleat-Black/White/Hyper',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/redSuperfly.jpg',
+                    name:
+                        'Nike Mercurial Superfly V FG Soccer Cleat - Bright Crimson/Black/University Red/Hyper Crimson ',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/redTiempo.jpg',
+                    name:
+                        'Nike Tiempo Legend VII FG Soccer Cleat - Univeristy Red/White/Black/Bright Crimson',
+                    price: '229.99'
+                },
+                {
+                    img_url: '/assets/photos/blueHypervenom.jpg',
+                    name:
+                        'Nike Hypervenom Phantom III DF FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueMagista.jpg',
+                    name:
+                        'Nike Magista Obra II FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueSuperfly.jpg',
+                    name:
+                        'Nike Mercurial Superfly V FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueTiempo.jpg',
+                    name:
+                        'Nike Tiempo Legend VII FG Soccer Cleat - Gamma Blue/White/Obsidian/Glacier Blue',
+                    price: '229.99'
+                }
+            ],
+            Clothing: [
+                {
+                    img_url: '/assets/photos/academypants.jpg',
+                    name: "Nike Dry Academy: Men's Soccer Pants",
+                    price: '45'
+                },
+                {
+                    img_url: '/assets/photos/stikepants.jpg',
+                    name: "Nike Dry Strike: Men's Soccer Pants",
+                    price: '45'
+                },
+                {
+                    img_url: '/assets/photos/squadpants.jpg',
+                    name: "Nike Dry Squad: Men's Soccer Pants",
+                    price: '70'
+                },
+                {
+                    img_url: '/assets/photos/CR7T-Shirt.jpg',
+                    name: "Nike Dry CR7: Men's Long Sleeve T-Shirt",
+                    price: '40'
+                },
+                {
+                    img_url: '/assets/photos/academyHoodie.jpg',
+                    name: "Nike Dry Academy: Men's Pullover Soccer Hoodie",
+                    price: '40'
+                },
+                {
+                    img_url: '/assets/photos/BarcaJersey.jpg',
+                    name:
+                        "2017/18 FC Barcelona Stadium Home: Men's Soccer Jersey",
+                    price: '90'
+                },
+                {
+                    img_url: '/assets/photos/psg.jpg',
+                    name:
+                        "2017/18 Paris Saint-Germain Stadium Home: Men's Soccer Jersey",
+                    price: '90'
+                },
+                {
+                    img_url: '/assets/photos/U.Sjacket.jpg',
+                    name: "U.S. Authentic N98: Men's Track Jacket",
+                    price: '105'
+                }
+            ],
+            Gear: [
+                {
+                    img_url: '/assets/photos/nikeball.jpg',
+                    name: 'Nike Primier League Magia: Soccer Ball',
+                    price: '65'
+                },
+                {
+                    img_url: '/assets/photos/nikeglove.jpg',
+                    name: 'Nike HyperWarm Field Player: Soccer glove',
+                    price: '30'
+                },
+                {
+                    img_url: '/assets/photos/nikebackpack.jpg',
+                    name: 'Nike Club Team Swoosh: Backpack',
+                    price: '50'
+                },
+                {
+                    img_url: '/assets/photos/nikeshinguard.jpg',
+                    name: 'Nike Mecurial Lite: Soccer Shin-Guards',
+                    price: '22'
+                },
+                {
+                    img_url: '/assets/photos/nikekeeperglove.jpg',
+                    name: 'Nike Vapor Grip3 Goalkeeper: Soccer Gloves',
+                    price: '120'
+                },
+                {
+                    img_url: '/assets/photos/nikesoccersock.jpg',
+                    name: 'NikeGrip Strike Over-the-Calf: Soccer Socks',
+                    price: '36'
+                },
+                {
+                    img_url: '/assets/photos/BarcaBeanie.jpg',
+                    name: 'FC Barcelona: Beanie',
+                    price: '30'
+                },
+                {
+                    img_url: '/assets/photos/nikepump.jpg',
+                    name: 'Nike 3: Ball Pump',
+                    price: '10'
+                }
+            ]
+        }
+    ],
+    Addidas: [
+        {
+            cleat: [
+                {
+                    img_url: '/assets/photos/ACE17.jpg',
+                    name: 'ACE 17+ PURECONTROL FIRM GROUND CLEATS:',
+                    price: '300'
+                },
+                {
+                    img_url: '/assets/photos/NEMEZIZ17.jpg',
+                    name: 'NEMEZIZ 17+ 360 AGILITY FIRM GROUND CLEATS',
+                    price: '320'
+                },
+                {
+                    img_url: '/assets/photos/redSuperfly.jpg',
+                    name: '',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/redTiempo.jpg',
+                    name:
+                        'Nike Tiempo Legend VII FG Soccer Cleat - Univeristy Red/White/Black/Bright Crimson',
+                    price: '229.99'
+                },
+                {
+                    img_url: '/assets/photos/blueHypervenom.jpg',
+                    name:
+                        'Nike Hypervenom Phantom III DF FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueMagista.jpg',
+                    name:
+                        'Nike Magista Obra II FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueSuperfly.jpg',
+                    name:
+                        'Nike Mercurial Superfly V FG Soccer Cleat - Thunder Blue/Glacier Blue/Gamma Blue/White',
+                    price: '299.99'
+                },
+                {
+                    img_url: '/assets/photos/blueTiempo.jpg',
+                    name:
+                        'Nike Tiempo Legend VII FG Soccer Cleat - Gamma Blue/White/Obsidian/Glacier Blue',
+                    price: '229.99'
+                }
+            ],
+            Clothing: [
+                {
+                    img_url: '/assets/photos/academypants.jpg',
+                    name: "Nike Dry Academy: Men's Soccer Pants",
+                    price: '45'
+                },
+                {
+                    img_url: '/assets/photos/stikepants.jpg',
+                    name: "Nike Dry Strike: Men's Soccer Pants",
+                    price: '45'
+                },
+                {
+                    img_url: '/assets/photos/squadpants.jpg',
+                    name: "Nike Dry Squad: Men's Soccer Pants",
+                    price: '70'
+                },
+                {
+                    img_url: '/assets/photos/CR7T-Shirt.jpg',
+                    name: "Nike Dry CR7: Men's Long Sleeve T-Shirt",
+                    price: '40'
+                },
+                {
+                    img_url: '/assets/photos/academyHoodie.jpg',
+                    name: "Nike Dry Academy: Men's Pullover Soccer Hoodie",
+                    price: '40'
+                },
+                {
+                    img_url: '/assets/photos/BarcaJersey.jpg',
+                    name:
+                        "2017/18 FC Barcelona Stadium Home: Men's Soccer Jersey",
+                    price: '90'
+                },
+                {
+                    img_url: '/assets/photos/psg.jpg',
+                    name:
+                        "2017/18 Paris Saint-Germain Stadium Home: Men's Soccer Jersey",
+                    price: '90'
+                },
+                {
+                    img_url: '/assets/photos/U.Sjacket.jpg',
+                    name: "U.S. Authentic N98: Men's Track Jacket",
+                    price: '105'
+                }
+            ],
+            Gear: [
+                {
+                    img_url: '/assets/photos/nikeball.jpg',
+                    name: 'Nike Primier League Magia: Soccer Ball',
+                    price: '65'
+                },
+                {
+                    img_url: '/assets/photos/nikeglove.jpg',
+                    name: 'Nike HyperWarm Field Player: Soccer glove',
+                    price: '30'
+                },
+                {
+                    img_url: '/assets/photos/nikebackpack.jpg',
+                    name: 'Nike Club Team Swoosh: Backpack',
+                    price: '50'
+                },
+                {
+                    img_url: '/assets/photos/nikeshinguard.jpg',
+                    name: 'Nike Mecurial Lite: Soccer Shin-Guards',
+                    price: '22'
+                },
+                {
+                    img_url: '/assets/photos/nikekeeperglove.jpg',
+                    name: 'Nike Vapor Grip3 Goalkeeper: Soccer Gloves',
+                    price: '120'
+                },
+                {
+                    img_url: '/assets/photos/nikesoccersock.jpg',
+                    name: 'NikeGrip Strike Over-the-Calf: Soccer Socks',
+                    price: '36'
+                },
+                {
+                    img_url: '/assets/photos/BarcaBeanie.jpg',
+                    name: 'FC Barcelona: Beanie',
+                    price: '30'
+                },
+                {
+                    img_url: '/assets/photos/nikepump.jpg',
+                    name: 'Nike 3: Ball Pump',
+                    price: '10'
+                }
+            ]
+        }
+    ]
+};
+
 //**************************** image slide function ******************/
 var myIndex = 0;
 
